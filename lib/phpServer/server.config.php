8b1dd6921c9ed1327a47b92690be4c3466e23bc3
Simeon Ivaylov Petrov <petrov.simeon.sipy@gmail.com>
2014-05-01 16:12:29 +0200

    updated code comments

--- lib/phpServer/server.config.php
+++ lib/phpServer/server.config.php
@@ -21,37 +21,37 @@
  * along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
 */
 
-    //debug flag, if true a server log will be outputted in the server console
+    //Debug flag, if true a server log will be outputted in the server console.
     define("DEBUG", true);
-    //stateful flag that determines whether the Web application is stateful or not
+    //Stateful flag that determines whether the Web application is stateful or not If it is set to true the <strong class="mdRwd">mdRwd</strong> WebSocket server stateful parameter must be set to true, too.
     define("STATEFUL", true);
-    //timeout in seconds for the component state lock
+    //Component lock timeout in seconds. If the server is locked because of a component and if it not receive the component state set before the LOCK_TIMEOUT, it releases the lock.
     define("LOCK_TIMEOUT", 5);
-    //WebSocket server IP
+    //WebSocket server IP.
     define("WS_SERVER_IP", "149.132.157.136");
-    //WebSocket server port
+    //WebSocket server port.
     define("WS_SERVER_PORT", 9000);
-    //maximum amount of virtual channels
+    //Maximum amount of virtual channels.
     define("WS_MAX_VCS", 10);
-    //maximum amount of clients that can be connected at one time in the same virtual channel
+    //Maximum amount of clients that can be connected at one time in the same virtual channel.
     define("WS_MAX_VC_CLIENTS", 5);
-    //maximum amount of clients that can be connected at one time in the same virtual channel on the same ip v4 address
+    //Maximum amount of clients that can be connected at one time in the same virtual channel on the same IP address.
     define("WS_MAX_VC_CLIENTS_PER_IP", 4);
-    //maximum amount of clients + 1 that can be connected at one time (leave this expression as it is)
+    //Maximum amount of clients + 1 that can be connected at one time (leave this expression as it is).
     define("WS_MAX_CLIENTS", WS_MAX_VCS * WS_MAX_VC_CLIENTS + 1);
-    //maximum amount of clients + 1 that can be connected at one time (leave this expression as it is)
+    //Maximum amount of clients + 1 that can be connected at one time (leave this expression as it is).
     define("WS_MAX_CLIENTS_PER_IP", WS_MAX_VCS * WS_MAX_VC_CLIENTS_PER_IP + 1);
-    //number of characters for the virtual channels ids
+    //Number of characters for the virtual channels ids.
     define("NUM_VC_ID_CHARS", 4);
-    //WebSoccket server allowed client origins
+    //WebSoccket server allowed client origins. Only listed origins will be allowed to connect to the WebSocket server.
     $ALLOWED_ORIGINS = array(
         "http://".WS_SERVER_IP,
         "http://mdrwd.org", "http://www.mdrwd.org",
         "http://mdrwd.net", "http://www.mdrwd.net",
         "http://mdrwd.com", "http://www.mdrwd.com"
     );
-    //closure status codes
-    //WebSocket protocol reserved status codes
+    //Closure status codes.
+    //WebSocket protocol reserved status codes.
     define("WS_STATUS_NORMAL_CLOSURE"              ,       1000);
     define("WS_STATUS_GONE_AWAY"                   ,       1001);
     define("WS_STATUS_PROTOCOL_ERROR"              ,       1002);
@@ -64,14 +64,14 @@
     define("WS_STATUS_MESSAGE_TOO_BIG"             ,       1009);
     define("WS_STATUS_MANDADORY_EXT"               ,       1010);
     define("WS_STATUS_TIMEOUT"                     ,       3000);
-    //mdRwd reserved status codes
-    //mdRwd ok status code
+    //mdRwd reserved status codes.
+    //mdRwd ok status code.
     define("WS_STATUS_OK"                          ,       5000);   
-    //mdRwd error status codes
+    //mdRwd error status codes.
     define("WS_STATUS_INVALID_MESSAGE"             ,       6000);  
     define("WS_STATUS_CLIENT_NOT_IN_VC"            ,       6001);  
     define("WS_STATUS_APP_NOT_STATEFUL"            ,       6002);  
-    //mdRwd warning status codes
+    //mdRwd warning status codes.
     define("WS_STATUS_UNKNOWN_VC"                  ,       7000);  
     define("WS_STATUS_CLIENT_ALREADY_IN_VC"        ,       7001);  
     define("WS_STATUS_MAX_VCS"                     ,       7002); 
@@ -79,6 +79,6 @@
     define("WS_STATUS_MAX_VC_CLIENTS_PER_IP"       ,       7004); 
     define("WS_STATUS_COMPONENT_LOCK"              ,       7005); 
     define("WS_STATUS_MAX_VC_LAYOUTS"              ,       7006); 
-    //you can set your own custom status codes, the range must be greater then 5000 because status codes below 5000 are reserved to the WebSocket protocol
+    //You can set your own custom status codes, the range must be greater then 5000 because status codes below 5000 are reserved to the WebSocket protocol.
 
 ?>
